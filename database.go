32f313010f2979c5790a4e3d81d6c5c36a514c84
debuconnor <gterppw@gmail.com>
2022-06-10 17:06:58 +0900

    minified db connection

--- database.go
+++ database.go
@@ -6,23 +6,10 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func Initialize(file string) (*sql.DB, error){
+func Save(file string, r Reservation) error{
 	db, err := sql.Open("sqlite3", file)
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
-}
-
-func InsertData(db *sql.DB, r Reservation) error{
-	tx, _ := db.Begin()
-	stmt, _ := tx.Prepare("insert into reservation (id, name, tel, place, date) values (?, ?, ?, ?, ?)")
-	_, err := stmt.Exec(r.Id, r.Name, r.Tel, r.Place, r.Date)
-	if err != nil {
-		return err
-	}
-
-	tx.Commit()
-	return nil
+	stmt, _ := db.Prepare("insert into reservation (id, name, tel, place, date) values (?, ?, ?, ?, ?)")
+	stmt.Exec(r.Id, r.Name, r.Tel, r.Place, r.Date)
+	
+	return err
 }
