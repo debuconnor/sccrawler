54fc341fb7fbde583fec292b067addf469b2a6b4
debuconnor <gterppw@gmail.com>
2023-01-22 23:08:49 +0900

    1. change unnecessary navigation 2. add test func

--- crawler.go
+++ crawler.go
@@ -1,16 +1,20 @@
 package sccrawler
 
 import (
-	"time"
 	"context"
+	"fmt"
+	"strconv"
+	"time"
+
 	"github.com/chromedp/chromedp"
 )
 
-func Start(email, pw, pageNum string, c chan<- string) {
+// Run crwaler to get SC reservations
+func Start(email, pw, pageNum string, c chan<- string, channum int) {
 	var result string
 
-	opts :=append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.DisableGPU,		
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.DisableGPU,
 		chromedp.Flag("headless", true),
 	)
 	ctx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
@@ -18,25 +22,59 @@ func Start(email, pw, pageNum string, c chan<- string) {
 
 	ctx, cancel = chromedp.NewContext(ctx)
 	defer cancel()
-	ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
+	ctx, cancel = context.WithTimeout(ctx, 15*time.Second)
 	defer cancel()
 
 	err := chromedp.Run(ctx,
-						chromedp.Navigate(`https://partner.spacecloud.kr/auth/login`),
-						chromedp.WaitVisible(`#email`),
-						chromedp.WaitVisible(`#pw`),
-						chromedp.SendKeys(`#email`, email, chromedp.ByID),
-						chromedp.SendKeys(`#pw`, pw, chromedp.ByID),
-						chromedp.Click(`.member_inner > form > fieldset > button`, chromedp.NodeVisible),
-						chromedp.Sleep(1*time.Second),
-						chromedp.Navigate(`https://partner.spacecloud.kr/reservation?page=`+pageNum),
-						chromedp.Sleep(1*time.Second),
-						chromedp.OuterHTML(`.list_box_wrap`, &result, chromedp.NodeVisible),
-					)
+		chromedp.Navigate("https://partner.spacecloud.kr/auth/login"),
+		chromedp.WaitVisible("section.member_wrap"),
+		chromedp.SendKeys("#email", email, chromedp.ByQuery),
+		chromedp.SendKeys("#pw", pw, chromedp.ByQuery),
+		chromedp.Click(".member_inner > form > fieldset > button", chromedp.NodeVisible),
+		chromedp.Sleep(1*time.Second),
+		chromedp.WaitVisible("#all_menu", chromedp.ByQuery),
+		chromedp.Navigate("https://partner.spacecloud.kr/reservation?RSV_STAT_CD=RSCMP&page="+pageNum),
+		chromedp.WaitVisible(".list_box_wrap", chromedp.ByQuery),
+		chromedp.OuterHTML(".list_box_wrap", &result, chromedp.NodeVisible),
+	)
 
-	if err != nil{
-		panic(err)
+	if err != nil {
+		output := time.Now().String() + "\nError chan# " + strconv.Itoa(channum) + "\n" + err.Error()
+		panic(output)
 	}
 
 	c <- result
+}
+
+func ChromedpTest(email, pw string) {
+	fmt.Println("*** chromedp Test ***")
+	var result string
+
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("headless", true),
+	)
+	ctx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer cancel()
+
+	ctx, cancel = chromedp.NewContext(ctx)
+	defer cancel()
+	ctx, cancel = context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(`https://partner.spacecloud.kr/auth/login`),
+		chromedp.WaitVisible(`#email`),
+		chromedp.WaitVisible(`#pw`),
+		chromedp.SendKeys(`#email`, email, chromedp.ByID),
+		chromedp.SendKeys(`#pw`, pw, chromedp.ByID),
+		chromedp.Click(`.member_inner > form > fieldset > button`, chromedp.NodeVisible),
+		chromedp.WaitVisible("#all_menu"),
+		chromedp.OuterHTML(`.text-info`, &result, chromedp.NodeVisible),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(result)
 }
